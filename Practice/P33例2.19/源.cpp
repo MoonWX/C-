35c1542969fbd4c749facac1dfeec2b55b129d95
TAOGLA <wangxiao3027@vip.qq.com>
2022-09-03 16:52:58 +0800

    Update 2022.9.3

---
+++ Practice/P33例2.19/源.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int sign = 1;
+	double fenmu = 2.0, sum = 1.0, temp;
+	while (fenmu <= 100)
+	{
+		sign = -sign;
+		temp = sign / fenmu;
+		sum = sum + temp;
+		fenmu = fenmu + 1;
+	}
+	printf("%f\n", sum);
+	return 0;
+}
