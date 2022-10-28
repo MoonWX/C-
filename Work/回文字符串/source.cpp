e6247990b0f39a0228d4fe73a5ce5d3fb05804ef
MoonWX <wangxiao3027@vip.qq.com>
2022-10-28 11:52:15 +0800

    Update 2022.10.28

---
+++ Work/回文字符串/source.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int check(char* str)
+{
+	int i = 0;
+	int j = strlen(str) - 1;
+	while (i < j)
+	{
+		if (*(str + i) != *(str + j))
+			return 0;
+		i++;
+		j--;
+	}
+	return 1;
+}
+
+void get_str(char* str)
+{
+	int n = strlen(str) - 1;
+
+	char* p = new char[n + 1];
+	strcpy(p, str);
+	int i = 0;
+	int j;
+	for (; i < n; i++)
+	{
+		p += i;
+		j = n - i;
+		while (j > 0){
+			*(p + j + 1) = '\0';
+			if (check(p))
+			{
+				cout << p << "   ";
+				cout << endl;
+			}
+			j--;
+		}
+		strcpy(p, str);
+	}
+}
+int main()
+{
+	char s[505];
+	cin.getline(s, 505);
+	get_str(s);
+	return 0;
+}
